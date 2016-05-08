fbde0635208d793d2e3c9eebb1cbc8fab7fe6125
James Wade <jpswade@gmail.com>
2016-05-08 14:04:10 +0100

    Update the VNC script

--- setup_autostart_vnc.sh
+++ setup_autostart_vnc.sh
@@ -10,25 +10,29 @@ fi
 
 hash x11vnc 2>/dev/null || { echo >&2 "x11vnc command not found."; exit 1; }
 
-if [ ! -f ~/.x11vnc/passwd ]; then
-    x11vnc -storepasswd "password" ~/.x11vnc/passwd
+# Create for usepw
+if [ ! -f ~/.vnc/passwd ]; then
+    mkdir -p ~/.vnc
+    x11vnc -storepasswd "raspberry" ~/.vnc/passwd
 fi
 
+# Create autostart
 if [ ! -f ~/.config/autostart/x11vnc.desktop ]; then
-    mkdir ~/.config/autostart
+    mkdir -p ~/.config/autostart
     cat >> ~/.config/autostart/x11vnc.desktop <<EOT
     [Desktop Entry]
     Encoding=UTF-8
     Type=Application
     Name=X11VNC Daemon
     Comment=Share this desktop by VNC
-    Exec=x11vnc -forever -usepw -httpport 5900 -display :0 -ultrafilexfer
+    Exec=x11vnc -forever -usepw -httpport 5900 -display :0 -ultrafilexfer -ncache -ncache 10
     StartupNotify=false
     Terminal=false
     Hidden=false"
 EOT
 fi
 
+# @see http://askubuntu.com/questions/5172/running-a-desktop-file-in-the-terminal
 gtk-launch x11vnc
 
 #EOF
