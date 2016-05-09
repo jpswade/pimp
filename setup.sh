e2d288bd838a6df37e841de22f9fbd41e5cbb981
James Wade <jpswade@gmail.com>
2016-05-09 22:43:37 +0100

    Run all the scripts and setup the LAN

--- setup.sh
+++ setup.sh
@@ -20,7 +20,26 @@ ntpdate -u pool.ntp.org
 # Let's setup speech using festival, just for fun!
 #grep 'festival' /etc/rc.local || sed -i 's/printf "My IP address is %s\n" "$_IP"/printf "My IP address is %s\n" "$_IP" | festival --tts' /etc/rc.local
 
+# Setup LAN.
+sed -i 's/iface eth0 inet manual/iface eth0 inet dhcp/g' /etc/network/interfaces && ifdown eth0 && ifup eth0
+
+# Setup Wifi.
+sh setup_wifi.sh
+
+# Setup VNC.
+sh setup_autostart_vnc.sh
+
+# Setup Music Player.
+sh setup_music_player.sh
+
+# Setup BlueTooth Audio.
+sh setup_bluetooth_audio.sh
+
 # Setup Startup script.
 grep "$CWD/startup.sh" /etc/rc.local || sudo sed -i -e '$i \sh '$CWD'/startup.sh &\n' /etc/rc.local
 chmod +x "$CWD/startup.sh"
+
+# Start.
+sh startup.sh
+
 #EOF
